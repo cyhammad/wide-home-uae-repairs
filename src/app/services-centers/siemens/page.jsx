36c7f5c5e06d48307cb8ddb2334d36d06650a346
cyhammad <hammad727272@gmail.com>
2024-05-05 00:55:42 +0500

    changes done'

--- src/app/services-centers/siemens/page.jsx
+++ src/app/services-centers/siemens/page.jsx
@@ -1,12 +1,12 @@
 import Footer from "@/components/Footer";
 import LogoSection from "@/components/LogoSection";
 import TopBar from "@/components/TopBar";
-import SiemensAboutUs from "@/components/siemens/SiemensAboutUs";
-import SiemensExperience from "@/components/siemens/SiemensExperience";
-import SiemensMain from "@/components/siemens/SiemensMain";
-import SiemensQualities from "@/components/siemens/SiemensQualities";
-import SiemensServicesBar from "@/components/siemens/SiemensServiceBar";
-import SiemensServices from "@/components/siemens/SiemensServices";
+import SiemensAboutUs from "@/components/Siemens/SiemensAboutUs";
+import SiemensExperience from "@/components/Siemens/SiemensExperience";
+import SiemensMain from "@/components/Siemens/SiemensMain";
+import SiemensQualities from "@/components/Siemens/SiemensQualities";
+import SiemensServicesBar from "@/components/Siemens/SiemensServiceBar";
+import SiemensServices from "@/components/Siemens/SiemensServices";
 
 const SiemensPage = () => {
   return (
